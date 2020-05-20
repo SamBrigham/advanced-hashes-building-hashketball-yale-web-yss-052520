5799e0ef645083ab3801738732a6d12635ed69c4
Samuel Brigha <sam.brigham@yale.edu>
2020-05-20 17:49:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,7 +11,7 @@ game_hash = {
           ,
       [:player_name => "Brook Lopez", :number => 11, :shoe => 17, :points => 17, :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_dunks => 15]
           ,
-      [ :player_name => , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks =>]
+      [ :player_name => "Mason Plumlee", :number => 1, :shoe => 19, :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks =>]
         ,
       [:player_name => , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks =>   
         ]
