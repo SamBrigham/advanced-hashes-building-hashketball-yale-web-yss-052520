c0fe7b57406f7210df5b3850fda00f4c618a289c
Samuel Brigha <sam.brigham@yale.edu>
2020-05-20 17:28:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -19,37 +19,3 @@ end
 
 
 
-
-
-
-:player_name => ,
-:number => ,
-:shoe => ,
-:points => ,
-:rebounds => ,
-:assists => ,
-:steals => ,
-:blocks => ,
-:slam_dunks =>
-
-      ]
-  },
-  :away => {
- :team_name => "",
-    :colors => [,],
-    :players => [
-:player_name => ,
-:number => ,
-:shoe => ,
-:points => ,
-:rebounds => ,
-:assists => ,
-:steals => ,
-:blocks => ,
-:slam_dunks => 
-]
-}
-    
-  }
-  
-end  
