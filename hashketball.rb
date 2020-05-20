28d9723b73f1146646d8d5fbbf3f673f04135c6e
Samuel Brigha <sam.brigham@yale.edu>
2020-05-20 17:51:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -21,7 +21,7 @@ game_hash = {
     :team_name => "Charlotte Hornets",
     :colors => ["Torquoise","Purple"],
     :players => [
-      [:player_name => , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => ]
+      [:player_name => "Jeff Adrien", :number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => , :slam_dunks => ]
         ,
       [:player_name => , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => 
           ]
